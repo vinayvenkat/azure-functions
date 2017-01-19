f7f53e100743624d50aa818c055c48fc8a39e327
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-19 11:10:20 -0800

    Adding the queue triggers and timer modifications

--- vv_timer_func/index.js
+++ vv_timer_func/index.js
@@ -4,6 +4,8 @@ module.exports = function (context, myTimer) {
     context.log('Node.js timer trigger function ran!', timeStamp);   
     context.log(GetEnvironmentVariable("AzureWebJobsStorage"));
     context.log(GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+	var message = 'Service Bus queue message created at ' + timeStamp;
+	context.bindings.myQueue = message;	
 
     context.done();
 };
