5909e69fc71209b3b702ed2c23ef547b68eb7f0a
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-19 13:03:27 -0800

    Print extra env variable

--- vv_timer_func/index.js
+++ vv_timer_func/index.js
@@ -4,6 +4,7 @@ module.exports = function (context, myTimer) {
     context.log('Node.js timer trigger function ran!', timeStamp);   
     context.log(GetEnvironmentVariable("AzureWebJobsStorage"));
     context.log(GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+    context.log(GetEnvironmentVariable("AzureWebJobsEndpoint"));
 	var message = 'Service Bus queue message created at ' + timeStamp;
 	context.bindings.myQueue = message;	
 
