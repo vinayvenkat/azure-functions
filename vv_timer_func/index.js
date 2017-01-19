1a243b816abf574f1f4bb358cc23be218f8394f3
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-19 09:59:12 -0800

    Adding a timer triggered function to evaulate accessing environment variables

---
+++ vv_timer_func/index.js
@@ -0,0 +1,14 @@
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    context.log('Node.js timer trigger function ran!', timeStamp);   
+    context.log(GetEnvironmentVariable("AzureWebJobsStorage"));
+    context.log(GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+
+    context.done();
+};
+
+function GetEnvironmentVariable(name)
+{
+    return name + ": " + process.env[name];
+}
